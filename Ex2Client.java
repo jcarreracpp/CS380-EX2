fff9c43a1963f97c0656f6820e22203ce1003ca7
Jorge Carrera <Jorge Carrera>
2017-04-12 23:19:37 -0700

    Cleaned up text formatting.

--- Ex2Client.java
+++ Ex2Client.java
@@ -4,17 +4,12 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.util.zip.CRC32;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Jorge
  */
 public class Ex2Client {
+
     public static void main(String[] args) throws Exception {
         try (Socket socket = new Socket("codebank.xyz", 38102)) {
             byte a;
@@ -22,49 +17,57 @@ public class Ex2Client {
             byte[] container = new byte[100];
             byte[] errorCode = new byte[4];
             long result;
-            
+
             System.out.println("Connected...");
-            
+
             InputStream is = socket.getInputStream();
             OutputStream os = socket.getOutputStream();
             CRC32 crc = new CRC32();
-            
+
             System.out.println("Recieved bytes: \n");
-            for(int i = 0; i < 100; i++){
-                a = (byte)is.read();
-                a = (byte)(a << 4);
-                b = (byte)is.read();
-                a = (byte)(a + b);
+            
+            for (int i = 0; i < 100; i++) {
+                a = (byte) is.read();
+                a = (byte) (a << 4);
+                b = (byte) is.read();
+                a = (byte) (a + b);
                 container[i] = a;
-                
-                if( ((i+1) % 10) != 0){
+
+                if (((i + 1) % 10) != 0) {
+                    
                     System.out.print(container[i] + "\t");
-                }else{
+                    
+                } else {
+                    
                     System.out.print(container[i] + "\t");
                     System.out.println();
+                    
                 }
-
+                
             }
-            
+
             crc.update(container);
             result = crc.getValue();
-            
+
             System.out.println("\nGenerated CRC32: " + result);
-            
-            os.write((byte)(result >>> 24));
-            os.write((byte)(result >>> 16));
-            os.write((byte)(result >>> 8));
-            os.write((byte)(result));
-            
-            if(is.read() == 1){
+
+            os.write((byte) (result >>> 24));
+            os.write((byte) (result >>> 16));
+            os.write((byte) (result >>> 8));
+            os.write((byte) (result));
+
+            if (is.read() == 1) {
+                
                 System.out.println("Response good.");
+                
             } else {
+                
                 System.out.println("Alignment failed!");
+                
             }
-            
+
             socket.close();
             System.out.println("Disconnected from server.");
         }
     }
-    
 }
